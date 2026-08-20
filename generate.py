9330d9055da82bd056abbb313699cbe59b247265
rushell <132660417+rushellxyz@users.noreply.github.com>
2026-08-20 10:46:49 +0300

    Since we dont have a json library, i might as well make it not a json

--- generate.py
+++ generate.py
@@ -5,13 +5,14 @@ print("Downloading...")
 response = requests.get("https://github.com/jimmyking9999999/gunsaw-level-editor-plus/raw/refs/heads/main/Levels.json", timeout=5)
 response.raise_for_status()
 data = response.json()
+
 print("Parsing...")
-with open("output.json", "w") as f:
-    f.write("{\n")
-    for i in data["levels"]:
-        checksum = hashlib.sha256(i["code"].encode('utf-8')).hexdigest()
-        name = i["name"].replace('"', '\\"')
-        f.write(f'  "{checksum}": "{name}",\n')
-    f.write("}")
+result = ""
+for i in data["levels"]:
+    checksum = hashlib.sha256(i["code"].encode('utf-8')).hexdigest()
+    name = i["name"].replace(',', '.')
+    result += (f'{checksum}, {name},')
 
-
+print("Saving...")
+with open("hashes.txt", "w") as f:
+    f.write(result[:-1])
